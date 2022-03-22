aa9227f82302d02e030a59389790b96146073dd5
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2022-03-21 20:49:07 -0400

    Create 🅿️.py

---
+++ 🅿️.py
@@ -0,0 +1,10 @@
+print("Welcome to da 🅿️ converter screw ur religon and shit and convert to 🅿️ 💀 \n \n")
+
+p_input = input("Speak your last words as they will get converted to 🅿️ > ")
+
+
+p_input = p_input.replace("p" , "🅿️")
+p_input = p_input.replace("P" , "🅿️")
+
+converted_txt = ("\n \n HAHA Let Me Show You 🅿️  Do It...")
+print("\n", converted_txt,"➡️➡️➡️", p_input)
